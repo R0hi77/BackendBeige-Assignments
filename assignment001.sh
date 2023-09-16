d74895226c6985f929b0060869183bfc72888fa5
Rohi P.J Apeswini Awinbe-Yisazuah <100289361+R0hi77@users.noreply.github.com>
2023-09-16 19:48:50 +0000

    Update assignment001.sh

--- assignment001.sh
+++ assignment001.sh
@@ -1,6 +1,4 @@
 #!bin/bash
 #write a bash/shell script to get the tcpdump of a process on your PC or entire system
 
-ifconfig | less  #shows all available network interfaces
-
-sudo tcpdump -i wlo1 -n #capturing network packets from  wlo1 interface
+sudo tcpdump -v -w packets.pcap
