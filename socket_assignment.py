c5ef1059d494855adfb3e60a01e5400f280176f3
Rohi P.J Apeswini Awinbe-Yisazuah <100289361+R0hi77@users.noreply.github.com>
2023-09-30 23:08:30 +0000

    socket_assignment.py

---
+++ socket_assignment.py
@@ -0,0 +1,56 @@
+import socket # socket module
+import configparser
+import threading
+ 
+def handle_client(ip,port):
+    print(f"Got connection from {ip}") 
+    ip.send(b"You have connected")
+    ip.close()
+
+
+#configuration file path
+beigefilepath ="cohort1.ini"
+
+#create a socket
+# AF_INET - IPv4 address family, SOCK_STREAM-  - TCP protocol
+s =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print("socket created sucessfully")
+
+# read the configuration file
+config = configparser.ConfigParser()
+config.read('assignment_configfile.ini') 
+
+#port number and host ip
+host = '127.0.0.1' 
+port = 5000
+server_address =  (host, port)
+
+# binding the socket to server address (ip, port
+s.bind(server_address) 
+
+#listening connections 
+s.listen() 
+
+
+while True:
+    (comm_socket, address) = s.accept()
+
+    try:
+
+        t=threading.Thread(target=handle_client,args=(comm_socket,address))
+
+        t.start()
+
+        data = comm_socket.recv(1024).decode('utf-8').strip()
+
+        with open(beigefilepath,mode='r')  as configfile:
+            if data + '\n' in configfile.readlines():
+                response ="STRING FOUND"
+                print(response)
+            else:
+                response = "STRING NOT FOUND"
+                print(response)
+
+        comm_socket.sendall(response.encode('utf-8'))
+    finally:
+        comm_socket.close()
