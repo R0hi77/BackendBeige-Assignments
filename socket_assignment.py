f5e4f6234df709e24b03f1582285564ab061ee1f
Rohi P.J Apeswini Awinbe-Yisazuah <100289361+R0hi77@users.noreply.github.com>
2023-10-02 13:04:53 +0000

    Update socket_assignment.py

--- socket_assignment.py
+++ socket_assignment.py
@@ -1,20 +1,22 @@
 import socket # socket module
 import configparser
 
-''' script to bind to a port and respond to connections '''
- 
+'''
+script by : Nana Kwadwo and Petersburg
+server script that binds a port and responds to connections
+'''
+
 #configuration file path
-beigefilepath = "/home/petersburg/BackEnd Beige class/socket programming/cohort1.txt"
+beigefilepath ="cohort1.txt"
 
 #create a socket
 # AF_INET - IPv4 address family, SOCK_STREAM-  - TCP protocol
-
 s =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 print("socket created sucessfully")
 
 # read the configuration file
 config = configparser.ConfigParser()
-config.read(open(beigefilepath))
+config.read(open(beigefilepath)) 
 
 #port number and host ip
 host = '127.0.0.1' 
@@ -24,29 +26,33 @@ server_address =  (host, port)
 # binding the socket to server address (ip, port
 s.bind(server_address) 
 
-#listening connections 
+#listening  for connections 
 s.listen() 
 
+#accepts connections
+(comm_socket, address) = s.accept()
 
 while True:
-    (comm_socket, address) = s.accept()
-
     try:
+       
         print(f"Got a connection from {comm_socket}")
-        comm_socket.send(b"You have connected")
-
+        comm_socket.send(b"connected to the server\n")
+     
+        # reaciving user input
+        comm_socket.send(b"Enter string :")
         data = comm_socket.recv(1024).decode('utf-8').strip()
 
+        # comparing user input to the config file content
         with open(beigefilepath,mode='r')  as configfile:
             if data + '\n' in configfile.readlines():
-                response ="STRING FOUND"
+                response ="STRING FOUND\n"
                 #print(response)
             else:
-                response = "STRING NOT FOUND"
+                response = "STRING NOT FOUND\n"
                 #print(response)
 
         comm_socket.sendall(response.encode('utf-8'))
     finally:
-        comm_socket.close()
-
+        #comm_socket.close()
+        print("should loop back\n")
 
